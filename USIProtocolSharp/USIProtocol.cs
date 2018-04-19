10db7dc72c5fe00c70d76ade7cebdb3fe8076272
ruhiel_murrue <ruhiel1983@gmail.com>
2018-04-19 23:01:56 +0900

    WIP

---
+++ USIProtocolSharp/USIProtocol.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USIProtocolSharp
+{
+
+    public class USIProtocol
+    {
+        public delegate void OnUSIOkEventHandler();
+
+        private Process _Process;
+
+        public event OnUSIOkEventHandler OnUSIOkEvent;
+
+        public USIProtocol()
+        {
+            _Process = new Process();
+
+            _Process.StartInfo.FileName = @"D:\ダウンロード\apery_sdt5\bin\apery_sdt5_bmi2.exe";
+
+            _Process.StartInfo.UseShellExecute = false;
+            _Process.StartInfo.RedirectStandardOutput = true; // 標準出力をリダイレクト
+            _Process.StartInfo.RedirectStandardInput = true; // 標準入力をリダイレクト
+            _Process.OutputDataReceived += new DataReceivedEventHandler(OnDataReceived);
+
+            _Process.Start();
+            _Process.BeginOutputReadLine();
+        }
+
+        public void USI() => _Process?.StandardInput.WriteLine("usi");
+
+        public void Quit() => _Process.StandardInput.WriteLine("quit");
+
+        // OutputDataReceivedイベントハンドラ. 行が出力されるたびに呼び出される
+        private void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Debug.WriteLine(e.Data);
+            if(e.Data == "usiok")
+            {
+                OnUSIOkEvent?.Invoke();
+            }
+        }
+
+        ~USIProtocol()
+        {
+            _Process?.Dispose();
+        }
+    }
+}
---
+++ USIProtocolSharpTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using USIProtocolSharp;
+
+namespace USIProtocolSharpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private USIProtocol c = new USIProtocol();
+        private bool flag = true;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            c.OnUSIOkEvent += C_OnUSIOkEvent;
+
+            c.USI();
+
+            while(flag)
+            {
+
+            }
+        }
+
+        private void C_OnUSIOkEvent()
+        {
+            c.Quit();
+
+            flag = false;
+        }
+    }
+}
